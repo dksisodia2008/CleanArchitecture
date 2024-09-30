3dd7ac92c6c6bea55856ca3839eaa626474d4f01
Dheeraj K <dksisodia2008@gmail.com>
2024-09-30 18:24:05 +0530

    Refactor the codes : Implemented Student repository instead of direct call dbconext in controller.

---
+++ ClearnArchitecture.Domain/IRepositories/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using ClearnArchitecture.Domain.Entities;
+
+namespace ClearnArchitecture.Domain.IRepositories
+{
+    public interface IStudentRepository
+    {
+
+        Task<List<Student>> GetAllStudentsAsync();
+        Task<Student?> GetStudentByIdAsync(int id);
+        Task<long> CreateAsync(Student student);
+        Task<bool> UpdateAsync(Student student);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> IsExitByIdAsync(int id);
+    }
+}
